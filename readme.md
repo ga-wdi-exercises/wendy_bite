ce9db78f8b58dc2f6cc71c3eaec5cbbb56d6fa65
Adrian Maseda <amaseda@gmail.com>
2016-08-25 17:00:10 -0400

    add tests + travis.yml

--- readme.md
+++ readme.md
@@ -2,12 +2,10 @@
 
 Your client, Wendy, has created "pixel-perfect" images representing how she wants her website to look, and provided the text. Make her site!
 
+Create a file called `style.css` in the root directory.
+
 She knows a thing or two about web design, so be sure your HTML and CSS validate.
 
 To check whether your CSS validates, the instructors will be using this site:
 
 https://jigsaw.w3.org/css-validator/#validate_by_input
-
-One solution:
-
-http://ga-wdi-exercises.github.io/wendy_bite/
---
+++ spec/wendybite_spec.js
@@ -0,0 +1,37 @@
+var fs = require("fs");
+var validator = require("w3c-css");
+var html = fs.readFileSync("index.html", "utf-8");
+
+describe("solution", function(){
+  it("links to a css file", function(){
+    expect(html.match("<link")).toNotBe(null);
+  })
+})
+
+describe("css", function(){
+  var solution = fs.readFileSync("style.css", "utf-8");
+  it("validates", function(done){
+    validator.validate({text: solution}, function(err, data){
+      expect(data.errors.length).toBe(0);
+      done();
+    })
+  });
+  it("uses font-family", function(){
+    expect(solution.match("font-family:")).toNotBe(null);
+  });
+  it("uses margin", function(){
+    expect(solution.match("margin")).toNotBe(null);
+  });
+  it("uses color", function(){
+    expect(solution.match("color:")).toNotBe(null);
+  });
+  it("uses padding", function(){
+    expect(solution.match("padding")).toNotBe(null);
+  });
+  it("uses border", function(){
+    expect(solution.match("border")).toNotBe(null);
+  });
+  it("uses float", function(){
+    expect(solution.match("float:")).toNotBe(null);
+  });
+});
